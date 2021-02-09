580035cef2c0dfa00af2830534ff20c7c1b84962
Rednaxelus <dacentrurus@protonmail.com>
2021-02-09 23:23:37 +0100

    refactored code a lot

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from PIL import Image, ImageDraw
-import reverse_geocoder as rg
-
 import json
 
-from shapely.geometry import mapping, shape
-from shapely.prepared import prep
+from PIL import Image, ImageDraw
 from shapely.geometry import Point
+from shapely.geometry import shape
+from shapely.prepared import prep
 
 
 def autocrop_image(image, border=0):
@@ -35,102 +33,140 @@ def autocrop_image(image, border=0):
     return cropped_image
 
 
-def get_unicode(code):
+def calc_flag_unicode(country_iso2):
+    if country_iso2 is None:
+        return None
     subchars = []
-    for c in code:
+    for c in country_iso2:
         res = ord(c.capitalize()) - ord('A') + 127462
         subchars.append(hex(res).upper()[2:])
-    return (subchars[0] + '-' + subchars[1] + '.png')
+    return subchars[0] + '-' + subchars[1] + '.png'
 
 
-def get_country(coordinates):
-    res3 = []
-    for pon in coordinates:
-        point = Point(pon[1], pon[0])  # lon, lat
-        found = False
-        for country, geom in countries.items():
+def create_coordinate_to_country_code_dict(coordinates):
+    pos_country_dict = {}
+    for pos in coordinates:
+        point = Point(pos[1], pos[0])  # lon, lat
+        found_country = None
+        for country_code, geom in all_countries_data.items():
             if geom.contains(point):
-                res3.append(get_unicode(country))
-                found = True
+                found_country = country_code
                 break
-        if not found:
-            res3.append(None)
-    return res3
+        pos_country_dict[pos] = found_country
+    return pos_country_dict
 
 
-if __name__ == '__main__':
-
+def calc_flag_height(flag_width):
     blueprint_flag = Image.open(f'openmoji-618x618-color/1F1E6-1F1E8.png')
     blueprint_flag = autocrop_image(blueprint_flag)
-
-    flag_width = 32
     scale_factor = flag_width / blueprint_flag.width
     flag_height = int(blueprint_flag.height * scale_factor)
+    return flag_height
 
-    blueprint_flag = blueprint_flag.resize((flag_width, flag_height), resample=Image.NEAREST)
 
-    latitude_start = 73.0  # 90
-    longitude_start = -13.0  # -180
-    step_lat = 0.5
-    step_lon = (flag_width / flag_height) * step_lat
-
-    latitude_div = 40  # 180
-    longitude_div = 55  # 360
-
-    draw_offset_x = 100
-    draw_offset_y = 100
-
+def create_base_image():
     map_width = int(flag_width * longitude_div / step_lon + draw_offset_x * 2)
     map_height = int(flag_height * latitude_div / step_lat + draw_offset_y * 2)
-    back_ground_color = (255, 255, 255)
-    im = Image.new("RGBA", (map_width, map_height), back_ground_color)
-    draw = ImageDraw.Draw(im)
+    background_color = (255, 255, 255)
+    im = Image.new("RGBA", (map_width, map_height), background_color)
+    ImageDraw.Draw(im)
+    return im
 
-    pos_arr2 = []
-    pos_arr = []
 
-    with open('countries.geojson') as f:
+def prepare_country_data(json_file):
+    with open(json_file) as f:
         data = json.load(f)
 
     countries = {}
     for feature in data["features"]:
         geom = feature["geometry"]
-        country = feature["properties"]["ISO_A2"]
-        countries[country] = prep(shape(geom))
+        country_code = feature["properties"]["ISO_A2"]
+        countries[country_code] = prep(shape(geom))
+    return countries
 
-    latitude_steps = 0
-    while latitude_steps < latitude_div:
-        longitude_steps = 0
-        while longitude_steps < longitude_div:
-            pos_arr.append((latitude_start - latitude_steps, longitude_start + longitude_steps))
-            longitude_steps += step_lon
-        latitude_steps += step_lat
-    pos_arr2 = get_country(pos_arr)
 
+def load_flags_for_country_codes(country_code_list):
     flag_dict = {}
+    for country_code in country_code_list:
+        if country_code not in flag_dict:
+            if country_code is not None:
+                flag_code = calc_flag_unicode(country_code)
+                if flag_code is not None:
+                    try:
+                        flag = Image.open(f'openmoji-618x618-color/{flag_code}')
+                        flag = autocrop_image(flag)
+                        flag = flag.resize((flag_width, flag_height), resample=Image.NEAREST)
+                        flag_dict[country_code] = flag
+                    except:
+                        pass
+    return flag_dict
 
-    array_pos = 0
 
+def create_coordinate_list():
+    coordinates = []
     latitude_steps = 0
     while latitude_steps < latitude_div:
         longitude_steps = 0
         while longitude_steps < longitude_div:
-            country = pos_arr2[array_pos]
-            if country is not None:
-                if country in flag_dict:
-                    chosen_flag = flag_dict[country]
-                else:
-                    chosen_flag = Image.open(f'openmoji-618x618-color/{country}')
-                    chosen_flag = autocrop_image(chosen_flag)
-                    chosen_flag = chosen_flag.resize((flag_width, flag_height), resample=Image.NEAREST)
-                    flag_dict[country] = chosen_flag
-                draw_x = int(draw_offset_x + longitude_steps * flag_width * 1 / step_lon - flag_width / 2)
-                draw_y = int(draw_offset_y + latitude_steps * flag_height * 1 / step_lat - flag_height / 2)
-                im.paste(chosen_flag, (draw_x, draw_y))
-            array_pos += 1
+            coordinates.append((latitude_start - latitude_steps, longitude_start + longitude_steps))
             longitude_steps += step_lon
         latitude_steps += step_lat
+    return coordinates
+
+
+def draw_images_on_base(base, images, draw_offset_x, draw_offset_y):
+    for image in images:
+        draw_x = int(draw_offset_x + image["offset_x"])
+        draw_y = int(draw_offset_y + image["offset_y"])
+        base.paste(image["image"], (draw_x, draw_y))
+    return base
+
+
+def create_image_data_array():
+    image_array = []
+    for position in coord_list:
+        country_code = pos_to_country_dict[position]
+        if country_code in flag_dict:
+            country_flag = flag_dict[country_code]
+            image_data = {}
+            image_data["image"] = country_flag
+            image_data["offset_x"] = abs(longitude_start - position[1]) * flag_width * 1 / step_lon - flag_width / 2
+            image_data["offset_y"] = abs(latitude_start - position[0]) * flag_height * 1 / step_lat - flag_height / 2
+            image_array.append(image_data)
+    return image_array
+
+
+if __name__ == '__main__':
+    draw_offset_x = 100
+    draw_offset_y = 100
+
+    flag_width = 32
+    flag_height = calc_flag_height(flag_width)
+
+    latitude_start = 90  # 73.0  # 90
+    longitude_start = -180  # -13.0  # -180
+    step_lat = 1  # 0.5
+    step_lon = (flag_width / flag_height) * step_lat
+
+    latitude_div = 180  # 40  # 180
+    longitude_div = 360  # 55  # 360
+
+    base_image = create_base_image()
+
+    all_countries_data = prepare_country_data('countries.geojson')
+
+    coord_list = create_coordinate_list()
+
+    pos_to_country_dict = create_coordinate_to_country_code_dict(coord_list)
+
+    existing_country_codes = pos_to_country_dict.values()
+
+    flag_dict = load_flags_for_country_codes(existing_country_codes)
+
+    image_array = create_image_data_array()
+
+    created_map = draw_images_on_base(base_image, image_array, draw_offset_x, draw_offset_y)
 
     # im.save('data/dst/rocket_pillow_paste_pos.jpg', quality=95)
 
-    im.show()
+    created_map.show()
